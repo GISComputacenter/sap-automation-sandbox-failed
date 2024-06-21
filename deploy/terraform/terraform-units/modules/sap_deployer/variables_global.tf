4e39c94a798191df947b092943bc6261eef024cf
CC SAP SDAF Repo <153024246+ccsapsdaf@users.noreply.github.com>
2024-06-21 17:29:23 +0100

    Update variables_global.tf

--- deploy/terraform/terraform-units/modules/sap_deployer/variables_global.tf
+++ deploy/terraform/terraform-units/modules/sap_deployer/variables_global.tf
@@ -137,10 +137,10 @@ variable "spn_id"                      { description = "SPN ID to be used for th
 variable "app_service"                 {
                                           description = "Details of the Application Service"
                                           default     = {}
-                                          validation {
-                                            condition = (
-                                              (var.app_service.use && length(trimspace(try(var.app_service.app_id, ""))) != 0 && length(trimspace(try(var.app_service.client_secret, ""))) != 0) || !var.app_service.use
-                                            )
-                                            error_message = "If using the Web App both the 'app_registration_app_id' and 'webapp_client_secret' variables must be defined."
-                                          }
+                                         # validation {
+                                         #   condition = (
+                                         #     (var.app_service.use && length(trimspace(try(var.app_service.app_id, ""))) != 0 && length(trimspace(try(var.app_service.client_secret, ""))) != 0) || !var.app_service.use
+                                         #   )
+                                         #   error_message = "If using the Web App both the 'app_registration_app_id' and 'webapp_client_secret' variables must be defined."
+                                         # }
                                        }
